35bb570c76971e5c1e96d6ab068f53d2e991b2ea
Paul Merrill <pdm@pdm.me>
2016-04-20 08:13:09 -0700

    First steps towards Apple frosted glass

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,27 @@
 
 #include "DirectoryTransform.h"
 #include "Image8.h"
-#include "MPIHelper.h"
 #include "Log.h"
+#include "filters/point-filters/ColorOverlay.h"
 #include "filters/point-filters/Saturation.h"
 
 using std::move;
 
+Image8 appleFrostedGlass(Image8 img) {
+    ParallelismPolicy policy = PP_CXX_STD_THREAD;
+
+    PointFilter desaturate = makeAlterSaturationFilter(0.5);
+    Image8 desaturatedImage = runPointFilter(move(img), desaturate, policy);
+
+    rgb nearWhite{245, 245, 245};
+    PointFilter overlay = makeColorOverlayFilter(nearWhite, 0.66);
+    return runPointFilter(move(desaturatedImage), overlay, policy);
+}
+
 int main(int argc, char **argv) {
     log("[main] Starting");
 
-    directoryTransform("test-images", "output-images",
-                       [](Image8 img) {
-                           PointFilter filter = makeAlterSaturationFilter(0.25);
-                           ParallelismPolicy policy = PP_CXX_STD_THREAD;
-                           return runPointFilter(move(img), filter, policy);
-                       });
+    directoryTransform("test-images", "output-images", appleFrostedGlass);
 
     log("[main] Finished");
 
